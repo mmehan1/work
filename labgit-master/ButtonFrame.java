82be3a6ab9ac0ce1b46adb94c747eed16b73e1bf
mmehan1 <mmehan@mac.com>
2016-01-27 22:51:27 -0700

    extra commit 1

--- labgit-master/ButtonFrame.java
+++ labgit-master/ButtonFrame.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-//I added a comment line here
+//I added a comment line here, and some more words here.
 class ButtonListener implements ActionListener
 {
 	int count = 0;
